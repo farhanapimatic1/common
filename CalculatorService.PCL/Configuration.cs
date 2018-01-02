f22dbd3839d1bdda646deac1cfb76d9598a0a689
Unknown <git@apimatic.io>
2018-01-02 10:39:26 +0000

    q

---
+++ CalculatorService.PCL/Configuration.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Text;
+using CalculatorService.PCL.Utilities;
+using CalculatorService.PCL.Models;
+namespace CalculatorService.PCL
+{
+    public partial class Configuration
+    {
+
+        public enum Environments
+        {
+            PRODUCTION,
+        }
+        public enum Servers
+        {
+            ICALCULATOR,
+        }
+
+        //The current environment being used
+        public static Environments Environment = Environments.PRODUCTION;
+
+        //A map of environments and their corresponding servers/baseurls
+        public static Dictionary<Environments, Dictionary<Servers, string>> EnvironmentsMap =
+            new Dictionary<Environments, Dictionary<Servers, string>>
+            {
+                { 
+                    Environments.PRODUCTION,new Dictionary<Servers, string>
+                    {
+                        { Servers.ICALCULATOR,"http://Example.org/ICalculator" },
+                    }
+                },
+            };
+
+        /// <summary>
+        /// Makes a list of the BaseURL parameters 
+        /// </summary>
+        /// <return>Returns the parameters list</return>
+        internal static List<KeyValuePair<string, object>> GetBaseURIParameters()
+        {
+            List<KeyValuePair<string, object>> kvpList = new List<KeyValuePair<string, object>>()
+            {
+            };
+            return kvpList; 
+        }
+
+        /// <summary>
+        /// Gets the URL for a particular alias in the current environment and appends it with template parameters
+        /// </summary>
+        /// <param name="alias">Default value:ICALCULATOR</param>
+        /// <return>Returns the baseurl</return>
+        internal static string GetBaseURI(Servers alias = Servers.ICALCULATOR)
+        {
+            StringBuilder Url =  new StringBuilder(EnvironmentsMap[Environment][alias]);
+            APIHelper.AppendUrlWithTemplateParameters(Url, GetBaseURIParameters());
+            return Url.ToString();        
+        }
+    }
+}
